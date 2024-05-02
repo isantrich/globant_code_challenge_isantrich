b17c9b8c925b2f7e1f5b613a39dafd10622c57db
isantrich <santrich.isabella@gmail.com>
2024-05-02 12:00:58 -0500

    Carga de datos mayor a 1000 agregada

--- testapi.py
+++ testapi.py
@@ -2,6 +2,7 @@ import requests
 import os
 import requests
 import json
+import pandas as pd
 
 def main():
     url = "http://localhost:5000/loadtransactions" 
@@ -12,17 +13,33 @@ def main():
             if filename.endswith(".csv"):
                 file_path = os.path.join(folder_path, filename)
 
-                with open(file_path, "r") as file:
-                    csv_data = file.read()
-                    
-                filename_without_extension = os.path.splitext(filename)[0]
-                datos = {"fileName": filename_without_extension, "fileData": csv_data}
-                print("este es el archivo de test")
-                print(filename_without_extension)
-                response = requests.post(url, data=json.dumps(datos), headers = {"Content-Type": "application/json"})
-                #response.raise_for_status() 
+                # Read the CSV file
+                df = pd.read_csv(file_path, header=None)
+                print(f"File {filename}:", len(df), "rows")
 
-                print(response.text)
+                # Divide the DataFrame into chunks of 1000 rows
+                chunks = [df[i:i+1000] for i in range(0, len(df), 1000)]
+                print("Total fragments:", len(chunks))
+
+                # Iterate over each chunk
+                for i, chunk in enumerate(chunks):
+                    start_row = i * 1000 + 1
+                    end_row = min((i + 1) * 1000, len(df))
+
+                    print(f"Fragment {i+1}: {len(chunk)} rows")
+                    print(f"Rows {start_row} to {end_row}")
+
+                    filename_without_extension = os.path.splitext(filename)[0]
+
+                    # Convert the chunk to CSV
+                    csv_data = chunk.to_csv(index=False, header=False)
+
+                    # Create the data dictionary
+                    datos = {"fileName": filename_without_extension, "fileData": csv_data}
+
+                    # Send the request
+                    response = requests.post(url, data=json.dumps(datos), headers={"Content-Type": "application/json"})
+                    print("API response:", response.text)
 
     except requests.exceptions.RequestException as e:
         print("Error making the request:", e)
